bc29cc229a0b4356516ca723e91d11d9cc7d66f1
polina-v80 <166666650+polina-v80@users.noreply.github.com>
2024-04-18 17:27:22 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,16 @@
+name = "Слово" # Тема урока
+#print(name, type(name))
+name = 5
+#print(name, type(name))
+name = 5.5
+#print(name, type(name))
+name = [1,2]
+#print(name, type(name))
+# Практическое задание
+name = "Polina"
+print("Name: " + name)
+age = 43
+print("Age:", age)
+print("Age:", age + 1)
+is_student = True
+print("Is Student:", is_student)
